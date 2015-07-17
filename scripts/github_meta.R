acd43302c059d1ba183da65449509b13ff565d0b
hafen <rhafen@gmail.com>
2015-07-17 11:10:24 -0400

    Update github meta script and add json

--- scripts/github_meta.R
+++ scripts/github_meta.R
@@ -6,10 +6,13 @@ create.github.context(personal_token = Sys.getenv("GITHUB_TOKEN"))
 
 yml <- yaml.load_file("_config.yml")
 
-lapply(yml$widgets, function(wdgt) {
-  # this returns paginated results - so it's capped at 30...
-  a <- get.stargazers(wdgt$ghuser, wdgt$ghrepo)
-  length(a$content)
-  # other meta data...
+meta <- lapply(yml$widgets, function(wdgt) {
+  a <- get.repository(wdgt$ghuser, wdgt$ghrepo)
+  a$content[c("stargazers_count", "open_issues_count", "forks_count", "watchers_count")]
 })
+
+names(meta) <- sapply(yml$widgets, function(x) paste(x$ghuser, x$ghrepo, sep = "_"))
+
+cat(toJSON(meta, auto_unbox = TRUE), file = "github_meta.json")
+
 
